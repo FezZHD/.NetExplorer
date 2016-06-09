1a5ebd96d69ee3fe32a9236a7673d581aa4b4655
FezZHD <zhenya535@gmail.com>
2016-06-10 02:34:32 +0300

    small refactoring again

--- netExplorer/netExplorer/ListItems.cs
+++ netExplorer/netExplorer/ListItems.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.RightsManagement;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace netExplorer
+﻿namespace netExplorer
 {
     public class ListItems
     {
--- netExplorer/netExplorer/MainClientWindow.xaml.cs
+++ netExplorer/netExplorer/MainClientWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Remoting.Channels;
-using System.Text;
 using System.Threading;
 using System.Windows;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using ListView = System.Windows.Controls.ListView;
 using ListViewItem = System.Windows.Controls.ListViewItem;
 using MessageBox = System.Windows.MessageBox;
